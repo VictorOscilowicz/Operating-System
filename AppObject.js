5a7256c82f3bc2b47c7c4d318ae965d305f1dee5
VictorOscilowicz <7voscilowicz@stpatrick.org>
2016-10-12 11:26:39 -0500

    Began New Assignment

---
+++ AppObject.js
@@ -0,0 +1,38 @@
+function App(n,m,s){
+
+    //Declare your instance variables here.
+    this.name=n;
+    this.memory=m;
+    this.state=s;
+
+    //Declare the instance function open here.
+    this.open = function(){
+
+
+    };
+    //Declare the instance function sleep here.
+    this.sleep = function(){
+      if(state=active){
+        state=sleep;
+        = (this.memory)*(.5);
+      }
+
+    };
+    //Declare the instance function active here.
+    this.active = function (){
+      g
+
+    };
+    //Declare the instance function close here.
+    this.close = function (){
+      g
+
+    };
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
