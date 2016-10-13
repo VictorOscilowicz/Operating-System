80de8609b02e1d9ee9c8a1ce37b62e2e3bb58642
VictorOscilowicz <7voscilowicz@stpatrick.org>
2016-10-13 10:25:40 -0500

    Continued work for this assignment.

--- AppObject.js
+++ AppObject.js
@@ -7,32 +7,43 @@ function App(n,m,s){
 
     //Declare the instance function open here.
     this.open = function(){
+      this.state=active
 
-
-    };
+    }
     //Declare the instance function sleep here.
     this.sleep = function(){
-      if(state=active){
-        state=sleep;
-        = (this.memory)*(.5);
+      if(this.state=active){
+        this.state=sleep;
+        this.memory=(this.memory)*(.5);
       }
 
-    };
+    }
     //Declare the instance function active here.
     this.active = function (){
-      g
+      if(this.state=sleep){
+        this.state=active;
+        this.memory=(this.memory*2);
+      }
 
-    };
+    }
     //Declare the instance function close here.
     this.close = function (){
-      g
+      if(this.state!=null){
+        this.state=null;
+        this.memory=0;
 
-    };
+      }
+
+    }
+
 }
 
 //Use the constructor to create 4 App objects below.
-
+var myApp = new App ("Gitbox",32,active);
+var hisApp = new App ("iMovie",64,active);
+var herApp = new App ("Github",18,sleep);
 
 
 //Declare an array named appList, and place all 4 of your apps in that array.
 //The order does not matter.
+g
